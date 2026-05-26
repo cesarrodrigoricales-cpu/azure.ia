d23c8e048959ed61f84dba8ac2a8ac8fd9f1b628
Cesar Ricales <cesarrodrigoricales@gmail.com>
2026-05-26 10:23:49 -0500

    cambios nuevos

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const app = express()
 app.use(express.json())
 app.use(express.static('public'))
 
-app.use('/api/ocr'           ,     require('./routes/ocrRoutes'))
-app.use('/api/imagen'        ,     require('./routes/imageRoutes'))
-app.use('/api/deteccion'     ,     require('./routes/deteccionRoutes'))
-app.use('/api/extraccion'    ,     require('./routes/extraccionRoutes'))
-app.use('/api/preguntas'   ,     require('./routes/preguntasRoutes'))
+app.use('/api/ocr', require('./routes/ocrRoutes'))
+app.use('/api/imagen', require('./routes/imageRoutes'))
+app.use('/api/deteccion', require('./routes/deteccionRoutes'))
+app.use('/api/extraccion', require('./routes/extraccionRoutes'))
+app.use('/api/preguntas', require('./routes/preguntasRoutes'))
 
-app.listen(3000, () => console.log('Servidor en http://localhost:3000'))
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`)
+})
